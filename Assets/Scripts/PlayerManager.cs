beb1d45f1e46de915d605945ffd86935ee9f9aa3
James Ferry <jamesferry84@hotmail.com>
2024-06-22 19:49:14 +0100

    added scenes and player cube test

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float xMove = .1f;
+    [SerializeField] private float yMove = .1f;
+    [SerializeField] private float zMove = .1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,9 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(xMove,yMove,zMove);
+        }
     }
 }
