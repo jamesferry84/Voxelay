c194369f4f9c4bd90f5f06560bcce3e08bdf9eff
James Ferry <jamesferry84@hotmail.com>
2024-06-23 16:50:05 +0100

    added rotational firing

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,10 +10,16 @@ public class PlayerManager : MonoBehaviour
 
     [SerializeField] private Projectile projectile;
 
+    private Projectile currentProjectile;
+    private Projectile currentProjectileTwo;
+
     // private float zValue = 0f;
     private float yValue = 0f;
     private float elapsed = 0f;
-    
+
+    private float maxHoldTime = 5f;
+    private float holdTime = 0f;
+    // private float angle;
 
     // Start is called before the first frame update
     void Start()
@@ -24,20 +30,66 @@ public class PlayerManager : MonoBehaviour
     void Update()
     {
         elapsed += Time.time;
-        if (elapsed >= firingDelay)
+       
+
+        if (Input.GetButton("Fire1"))
         {
-            if (Input.GetButton("Fire1"))
+            holdTime += Time.deltaTime;
+            holdTime = Mathf.Clamp(holdTime, 0, maxHoldTime);
+            if (elapsed >= firingDelay)
             {
-                Instantiate(projectile, transform.position, Quaternion.Euler(90,0,0));
+                FireProjectileOne();
+                FireProjectileTwo();
+                elapsed = 0f;
             }
 
-            elapsed = 0f;
+            
         }
-        
+        if (Input.GetButtonUp("Fire1") )
+        {
+            holdTime -= Time.deltaTime;
+            holdTime = Mathf.Clamp(holdTime, 0, maxHoldTime);
+            float angle = Mathf.Lerp(5, 180, holdTime / maxHoldTime);
+            if (angle <= 180f)
+            {
+                if (elapsed >= firingDelay)
+                {
+                    elapsed = 0f;
+                    Quaternion rotation = Quaternion.Euler(0,angle,0);
+                    currentProjectile = Instantiate(projectile, transform.position, rotation);
+                }
+            }
+            
+        }
+
 
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(xValue, yValue, zValue);
+    }
+
+    void FireProjectileOne()
+    {
+        float angle = Mathf.Lerp(180, 15, holdTime / maxHoldTime);
+        if (elapsed >= firingDelay)
+        {
+            //elapsed = 0f;
+            Quaternion rotation = Quaternion.Euler(0,angle,0);
+            currentProjectile = Instantiate(projectile, transform.position, rotation);
+        }
+    }
+    
+    void FireProjectileTwo()
+    { 
+        Debug.Log("Projectile Two");
+        float angle = Mathf.Lerp(180, 355, holdTime / maxHoldTime);
+        Debug.Log(angle);
+        if (elapsed >= firingDelay)
+        {
+            //elapsed = 0f;
+            Quaternion rotation = Quaternion.Euler(0,angle,0);
+            currentProjectileTwo = Instantiate(projectile, transform.position, rotation);
+        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+
+    [SerializeField] private bool rotateShoot = false;
+    
+    [SerializeField] Transform player;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
