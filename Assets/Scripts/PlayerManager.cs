9fea9d90a3faf454e4c935cd6a1ef2ec3ceb88de
James Ferry <jamesferry84@hotmail.com>
2024-06-29 10:28:12 +0100

    Adding rotation to player

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -28,6 +28,8 @@ public class PlayerManager : MonoBehaviour
     private float zValue;
 
     private float currentAngle = 0f;
+    public float rollFactor = -40f;
+
     void Start()
     {
     }
@@ -42,7 +44,7 @@ public class PlayerManager : MonoBehaviour
             holdTime = 0f;
             isHolding = true;
         }
-       
+
 
         if (Input.GetButton("Fire1"))
         {
@@ -54,10 +56,9 @@ public class PlayerManager : MonoBehaviour
                 FireProjectileTwo();
                 elapsed = 0f;
             }
-
-            
         }
-        if (Input.GetButtonUp("Fire1") )
+
+        if (Input.GetButtonUp("Fire1"))
         {
             isHolding = false;
             holdTime = 0f;
@@ -69,18 +70,21 @@ public class PlayerManager : MonoBehaviour
                 if (elapsed >= firingDelay)
                 {
                     elapsed = 0f;
-                    Quaternion rotation = Quaternion.Euler(0,angle,0);
+                    Quaternion rotation = Quaternion.Euler(0, angle, 0);
                     currentProjectile = Instantiate(projectile, transform.position, rotation);
                 }
             }
-            
         }
 
+        float xRoll = Input.GetAxis("Horizontal");
+        xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float roll = xRoll * rollFactor;
 
-         xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-         zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0f, 0f, roll);
 
-        transform.Translate(xValue, yValue, zValue);
+        // transform.Translate(xValue, 0, zValue);
+        transform.position = transform.position += new Vector3(xValue, 0, zValue);
     }
 
     void FireProjectileOne()
@@ -88,22 +92,23 @@ public class PlayerManager : MonoBehaviour
         float angle;
 
         angle = Mathf.Lerp(180, 15, holdTime / maxHoldTime);
-        
+
         if (!isHolding)
         {
             angle *= -1;
         }
+
         if (elapsed >= firingDelay)
         {
             //elapsed = 0f;
-            Quaternion rotation = Quaternion.Euler(0,angle,0);
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
             currentProjectile = Instantiate(projectile, transform.position, rotation);
             Vector3 direction = rotation * Vector3.forward;
             Vector3 playerVelocity = new Vector3(xValue, 0f, zValue) * Time.deltaTime;
             currentProjectile.Initialize(direction, playerVelocity);
         }
     }
-    
+
     void FireProjectileTwo()
     {
         float angle;
@@ -112,7 +117,7 @@ public class PlayerManager : MonoBehaviour
         if (elapsed >= firingDelay)
         {
             //elapsed = 0f;
-            Quaternion rotation = Quaternion.Euler(0,angle,0);
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
             currentProjectileTwo = Instantiate(projectile, transform.position, rotation);
             Vector3 direction = rotation * Vector3.forward;
             Vector3 playerVelocity = new Vector3(xValue, 0f, zValue) * Time.deltaTime;
